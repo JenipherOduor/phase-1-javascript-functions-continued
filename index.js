7c21c964d0fa34df77ddfa921c7a7815351c1892
Jenipher Oduor <jenipheroduor@gmail.com>
2023-07-07 19:51:28 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+function saturdayFun ( activity = "roller-skate")
+{
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun()
+saturdayFun("bowl")
+
+const mondayWork = function (toDo = "go to the office")
+{
+    return `This Monday, I will ${toDo}.`
+}
+mondayWork;
+mondayWork("work from home");
+
+const wrapAdjective  = function(style = "*")
+{
+    return function (degree = "special")
+    {
+        return `You are ${style}${degree}${style}!`
+    }
+}
